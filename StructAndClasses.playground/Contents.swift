acb6a4519a5979593ea732cbee99967eef3280a3
PetarSakotic <applesakota@gmail.com>
2019-10-18 10:42:01 +0200

    Zadatak 4

--- StructAndClasses.playground/Contents.swift
+++ StructAndClasses.playground/Contents.swift
@@ -21,7 +21,7 @@ func printValueFromArrayGuardStatement(array: [Int?]){
     print("****\nZadatak 1\nGuard statement\n****")
     for i in array{
         guard let value = i else{
-            return
+            continue
         }
         print("Int element is: \(value)")
     }
@@ -39,6 +39,47 @@ printValueFromArrayOptionalBinding(array: exampleArrayOptionalInt)
  kreiraj funkciju koja kao ulazni parametar ima niz ucenika, i na osnovu slucajnog odabira jednog ucenika, pozvace fukciju modela Ucenik "ucenik.igrajFizicko()", koja ce da ispise "{ime ucenika} igra {fudbal/odbojka)"
 */
 print("----\nZadatak 2\n----")
+class Student {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    
+    func playSport () -> String {
+        return "Which sport"
+    }
+    
+}
+class Male: Student {
+    
+    override func playSport() -> String {
+        
+        return "Football"
+    }
+}
+class Female: Student {
+    
+    override func playSport() -> String {
+        return "Vooleyball"
+    }
+}
+
+
+var pera = Male.init(name: "Pera")
+var ana = Female.init(name: "Ana")
+var mika = Male.init(name: "Mika")
+
+
+
+func printStudent (students: [Student]) {
+    let randomStudentIndex = Int.random(in: Range(uncheckedBounds: (0,students.count - 1)))
+    let randomStudent = students[randomStudentIndex]
+    print("\(randomStudent.name) igra \(randomStudent.playSport())")
+}
+
+printStudent(students: [pera,mika,ana])
+
+
 
 //Zadatak 3:
 /*
@@ -47,3 +88,43 @@ print("----\nZadatak 2\n----")
  kreiraj funkciju koja kao ulazni parametar ima niz ucenika, i na osnovu slucajnog odabira jednog ucenika, pozvace fukciju modela Ucenik "ucenik.igrajFizicko()", koja ce da ispise "{ime ucenika} igra {fudbal/odbojka)"
 */
 print("----\nZadatak 3\n----")
+
+protocol StudentProtocol {
+    var name: String { get set }
+    func igrajFizicko () -> String
+}
+
+struct MaleStruct: StudentProtocol {
+    var name: String
+    
+    func igrajFizicko() -> String {
+        return "Football"
+    }
+    
+}
+struct FemaleStruct: StudentProtocol {
+    var name: String
+    
+    func igrajFizicko() -> String {
+        return "Vooleyball"
+    }
+}
+
+var laza = MaleStruct(name: "Laza")
+var marija = FemaleStruct(name: "Marija")
+var ena = FemaleStruct(name: "Ena")
+
+
+
+
+func printStudentStruct (students: [StudentProtocol]) {
+    let randomStudentIndex = Int.random(in: Range(uncheckedBounds: (0,students.count - 1)))
+    let randomStudent = students[randomStudentIndex]
+    print("\(randomStudent.name) igra \(randomStudent.igrajFizicko())")
+}
+
+    
+printStudentStruct(students: [laza,marija,ena])
+    
+    
+
